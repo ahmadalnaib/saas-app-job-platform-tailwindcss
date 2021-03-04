582b58f388b7d78f158ffb43be69ed03566790af
ahmedhabeeb73 <alnaib888@gmail.com>
2021-03-04 18:21:23 +0100

    add tag manger

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,6 +5,14 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-MD6JP4X');</script>
+    <!-- End Google Tag Manager -->
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Fonts -->
@@ -17,6 +25,11 @@
     <script src="{{ asset('js/app.js') }}" defer></script>
 </head>
 <body>
+
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MD6JP4X"
+                  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 
 <!-- Section 1 -->
 <section class="w-full px-6 pb-12 antialiased bg-white">
@@ -81,6 +94,8 @@
 
     </div>
 </section>
+
+
 
 </body>
 </html>
---
+++ resources/views/newsletter.blade.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Fonts -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    <title>Document</title>
+</head>
+<body>
+
+<!-- Section 1 -->
+<section class="py-24 bg-white">
+
+    <div class="max-w-3xl px-10 mx-auto xl:px-5">
+
+        <div class="flex flex-col justify-center space-y-8">
+            <h2 class="w-full mx-auto text-4xl font-extrabold leading-none text-left text-gray-900 sm:text-5xl md:text-7xl">Weekly Newsletter</h2>
+            <p class="w-full max-w-3xl mx-auto text-xl text-left text-gray-500 md:text-2xl">Get the latest trends and updates in your inbox each week. We will provide you with top notch designs and resources.</p>
+
+            <div class="flex flex-col w-full mx-auto space-y-5 md:space-y-0 md:space-x-5 md:flex-row">
+                <form action="https://dundung.us1.list-manage.com/subscribe/post?u=97249b5ad9afc6f7dc04a9eb3&amp;id=367c4d4d3e" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" target="_blank" novalidate>
+                    <input type="hidden" name="b_97249b5ad9afc6f7dc04a9eb3_367c4d4d3e" tabindex="-1" value="">
+                <input name="EMAIL" type="email" class="flex-1 w-full px-5 py-5 text-2xl bg-gray-200 border border-gray-300 rounded-lg focus:ring-4 focus:border-blue-600 focus:ring-blue-600 focus:ring-opacity-50 focus:outline-none" placeholder="Email Address">
+                <button type="submit" value="Subscribe" name="subscribe" class="flex-shrink-0 px-10 py-5 text-2xl font-medium text-center text-white bg-blue-600 rounded-lg focus:ring-4 focus:ring-blue-600 focus:ring-opacity-50 focus:ring-offset-2 focus:outline-none">Sign Up</button>
+                    <form/>
+            </div>
+
+            <p class="text-gray-400"><span class="font-medium text-gray-700">Trusted by</span> over 27,000 subscribers.</p>
+
+        </div>
+
+    </div>
+
+</section>
+
+</body>
+</html>
+
+
