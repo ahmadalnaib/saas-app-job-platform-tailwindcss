e3e6218563910d78298582783015a27391b8acea
ahmedhabeeb73 <alnaib888@gmail.com>
2021-03-04 15:00:42 +0100

    first commit

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,84 @@
+<x-guest-layout>
+
+
+    <!-- Section 1 -->
+    <section class="w-full bg-white">
+
+        <div class="mx-auto max-w-7xl">
+            <div class="flex flex-col lg:flex-row">
+                <div class="relative w-full bg-cover lg:w-6/12 xl:w-7/12 bg-gradient-to-r from-white via-white to-gray-100">
+                    <div class="relative flex flex-col items-center justify-center w-full h-full px-10 my-20 lg:px-16 lg:my-0">
+                        <div class="flex flex-col items-start space-y-8 tracking-tight lg:max-w-3xl">
+                            <div class="relative">
+                                <p class="mb-2 font-medium text-gray-700 uppercase">Work smarter</p>
+                                <h2 class="text-5xl font-bold text-gray-900 xl:text-6xl">Features to help you work smarter</h2>
+                            </div>
+                            <p class="text-2xl text-gray-700">We've created a simple formula to follow in order to gain more out of your business and your application.</p>
+                            <a href="#_" class="inline-block px-8 py-5 text-xl font-medium text-center text-white transition duration-200 bg-blue-600 rounded-lg hover:bg-blue-700 ease">Get Started Today</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="w-full bg-white lg:w-6/12 xl:w-5/12">
+                    <div class="flex flex-col items-start justify-start w-full h-full p-10 lg:p-16 xl:p-24">
+                        <h4 class="w-full text-3xl font-bold">Signup</h4>
+                        <p class="text-lg text-gray-500">or, if you have an account you can <a href="{{route('login')}}" class="text-blue-600 underline">sign in</a></p>
+                        <div class="relative w-full mt-10 space-y-8">
+                            <!-- Validation Errors -->
+                            <x-auth-validation-errors class="mb-4" :errors="$errors" />
+
+                            <form method="POST" action="{{ route('register') }}">
+                            @csrf
+
+                            <!-- Name -->
+                                <div>
+                                    <x-label for="name" :value="__('Name')" />
+
+                                    <x-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus />
+                                </div>
+
+                                <!-- Email Address -->
+                                <div class="mt-4">
+                                    <x-label for="email" :value="__('Email')" />
+
+                                    <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
+                                </div>
+
+                                <!-- Password -->
+                                <div class="mt-4">
+                                    <x-label for="password" :value="__('Password')" />
+
+                                    <x-input id="password" class="block mt-1 w-full"
+                                             type="password"
+                                             name="password"
+                                             required autocomplete="new-password" />
+                                </div>
+
+                                <!-- Confirm Password -->
+                                <div class="mt-4">
+                                    <x-label for="password_confirmation" :value="__('Confirm Password')" />
+
+                                    <x-input id="password_confirmation" class="block mt-1 w-full"
+                                             type="password"
+                                             name="password_confirmation" required />
+                                </div>
+
+                                <div class="flex items-center justify-end mt-4">
+                                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
+                                        {{ __('Already registered?') }}
+                                    </a>
+
+                                    <x-button class="ml-4">
+                                        {{ __('Register') }}
+                                    </x-button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </section>
+
+</x-guest-layout>
